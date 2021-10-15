6c4783b5788e8f95c8f6cc7cb612e4b633d1417b
Guilherme dos Santos Silva <guisantossilva00@gmail.com>
2021-10-15 02:59:18 -0300

    criada a function renderizar produto e array listaProdutos

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ let listaProdutos = [
         preco: "99,99",
     },
     {
-        imagem :"imagens/produto.jpg",
-        titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
-        preco: "99,99",
+        imagem :"imagens/mousegamer.jpg",
+        titulo: "Mouse Gamer 2400 Dpi Vx Gaming Sark",
+        preco: "49,99",
     },
     {
-        imagem :"imagens/produto.jpg",
-        titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+        imagem :"imagens/kitvermelho.jpg",
+        titulo: "Kit Teclado Mouse Gamer Vermelho Vx Gaming Grifo",
         preco: "99,99",
     },
     {
@@ -29,5 +29,31 @@ function enviarEmail() {
 }
 
 function renderizarProdutos() {
-    let secaoProdutos = document.getElementById("espaco-mais-vendidos")
+    let secaoProdutos = document.getElementById("espaco-mais-vendidos");
+
+    let template = "";
+
+    for (let index = 0; index < listaProdutos.length; index++) {
+        const produto = listaProdutos[index];
+        
+        template += `<div class="container-card-externo">
+            <div class="container-card">
+                    <div>
+                        <img src="${produto.imagem}" alt="Imagem do Produto">
+                        <h3>${produto.titulo}</h3>
+                    </div>
+                    <div>
+                        <div class="preco-produto">
+                            R$ ${produto.preco}
+                        </div>
+                        <div>
+                            <span>À vista no pix</span>
+                        </div>
+                    </div>                               
+                    <a href="#">Comprar</a>
+            </div>                                                        
+        </div>`
+    }
+
+    secaoProdutos.innerHTML = template;
 }
