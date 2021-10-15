f5c72d62573eebcc52792fbbf881beec4065df4b
Guilherme dos Santos Silva <guisantossilva00@gmail.com>
2021-10-15 02:19:49 -0300

    Add produtos estilizados

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 let email = document.getElementById("campo-email");
 
+let listaProdutos = [
+    {
+        imagem :"imagens/produto.jpg",
+        titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+        preco: "99,99",
+    },
+    {
+        imagem :"imagens/produto.jpg",
+        titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+        preco: "99,99",
+    },
+    {
+        imagem :"imagens/produto.jpg",
+        titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+        preco: "99,99",
+    },
+    {
+        imagem :"imagens/produto.jpg",
+        titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+        preco: "99,99",
+    },
+]
+
 function enviarEmail() {
     let emailDoUsuario = email.value;
     console.log(emailDoUsuario);
+}
+
+function renderizarProdutos() {
+    let secaoProdutos = document.getElementById("espaco-mais-vendidos")
 }
