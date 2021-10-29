3dcbad2f38312e7e99f3e24a3a907738ee9b3f47
Guilherme dos Santos Silva <guisantossilva00@gmail.com>
2021-10-29 01:14:32 -0300

    Add outros produtos no js e html

--- js/script.js
+++ js/script.js
@@ -25,6 +25,72 @@ let listaProdutos = [
         preco: "99,99",
     },
     {
+        imagem :"imagens/headset.jpg",
+        titulo: "Headset Gamer Scorpion Com Fio Rgb Gh-x1000",
+        preco: "99,99",
+    },
+    // {
+    //     imagem :"imagens/produto.jpg",
+    //     titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+    //     preco: "99,99",
+    // },
+    // {
+    //     imagem :"imagens/produto.jpg",
+    //     titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+    //     preco: "99,99",
+    // },
+]
+
+let listaProdutosPerifericos = [
+    {
+        imagem :"imagens/produto.jpg",
+        titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+        preco: "99,99",
+    },
+    {
+        imagem :"imagens/mousegamer.jpg",
+        titulo: "Mouse Gamer 2400 Dpi Vx Gaming Sark",
+        preco: "49,99",
+    },
+    {
+        imagem :"imagens/kitvermelho.jpg",
+        titulo: "Kit Teclado Mouse Gamer Vermelho Vx Gaming Grifo",
+        preco: "99,99",
+    },
+    {
+        imagem :"imagens/produto.jpg",
+        titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+        preco: "99,99",
+    },
+    // {
+    //     imagem :"imagens/produto.jpg",
+    //     titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+    //     preco: "99,99",
+    // },
+    // {
+    //     imagem :"imagens/produto.jpg",
+    //     titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
+    //     preco: "99,99",
+    // },
+]
+
+let listaProdutosEletronicos = [
+    {
+        imagem :"imagens/smartwatch.jpg",
+        titulo: "Iwo 8 Lite Prata Relógio Smartwatch",
+        preco: "99,99",
+    },
+    {
+        imagem :"imagens/mousegamer.jpg",
+        titulo: "Mouse Gamer 2400 Dpi Vx Gaming Sark",
+        preco: "49,99",
+    },
+    {
+        imagem :"imagens/kitvermelho.jpg",
+        titulo: "Kit Teclado Mouse Gamer Vermelho Vx Gaming Grifo",
+        preco: "99,99",
+    },
+    {
         imagem :"imagens/produto.jpg",
         titulo: "Kit Teclado Mouse Gamer Azul Vx Gaming Grifo",
         preco: "99,99",
@@ -73,18 +139,65 @@ function renderizarProdutos() {
         </div>`
         
     }
-
     secaoProdutos.innerHTML = template;
-    console.log (secaoProdutos)
+}
 
-    // function showSlides(n) {
-    //     let i;
-    //     let slides = document.getElementById("slide");
-    //     if(n > slides.length) {slideIndex = 1}
-    //     if (n < 1) {slideIndex = slides.length};
-    //         for (i = 0; i < slide.length; i++) {
-    //             slide[i].style.display = "none";
-    //         }
-    //     slide[slideIndex-1].style.display = "block;"
-    // }
+function renderizarProdutosPerifericos() {
+    let secaoProdutosPerifericos = document.getElementById("espaco-perifericos");
+
+    let templatePerifericos = "";
+    
+    for (let index = 0; index < listaProdutosPerifericos.length; index++) {
+        const produtoPerifericos = listaProdutosPerifericos[index];
+        
+        templatePerifericos += `<div class="container-card-externo">
+            <div class="container-card">
+                    <div>
+                        <img src="${produtoPerifericos.imagem}" alt="Imagem do Produto">
+                        <h3>${produtoPerifericos.titulo}</h3>
+                    </div>
+                    <div>
+                        <div class="preco-produto">
+                            R$ ${produtoPerifericos.preco}
+                        </div>
+                        <div>
+                            <span>À vista no pix</span>
+                        </div>
+                    </div>                               
+                    <a href="#">Comprar</a>
+            </div>                                                        
+        </div>`
+        
+    }    
+    secaoProdutosPerifericos.innerHTML = templatePerifericos;
+}
+
+function renderizarProdutosEletronicos() {
+    let secaoProdutosEletronicos = document.getElementById("espaco-eletronicos");
+
+    let templateEletronicos = "";
+    
+    for (let index = 0; index < listaProdutosEletronicos.length; index++) {
+        const produtoEletronicos = listaProdutosEletronicos[index];
+        
+        templateEletronicos += `<div class="container-card-externo">
+            <div class="container-card">
+                    <div>
+                        <img src="${produtoEletronicos.imagem}" alt="Imagem do Produto">
+                        <h3>${produtoEletronicos.titulo}</h3>
+                    </div>
+                    <div>
+                        <div class="preco-produto">
+                            R$ ${produtoEletronicos.preco}
+                        </div>
+                        <div>
+                            <span>À vista no pix</span>
+                        </div>
+                    </div>                               
+                    <a href="#">Comprar</a>
+            </div>                                                        
+        </div>`
+        
+    }    
+    secaoProdutosEletronicos.innerHTML = templateEletronicos;
 }
